633df388832be0e80bae67807a68a349d2a3146d
Kiwiphoenix <98428865+kiwiphoenix364@users.noreply.github.com>
2022-02-12 16:22:52 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,16 @@
-//% color="#83de8a"
+//% color="#ff0808"
 namespace Targeting {
-    //% block="Find First Sprite Along Path: Type $spriteKind Distance $range From Sprite $input "
+    //% block="Find First Sprite Along Path: kind $spriteKind Distance $range From Sprite $input "
     //% spriteKind.shadow="spritekind"
-    export function FindFirstSpriteOfType(spriteKind: number, range: number, input: Sprite): any {
+    export function FindFirstSpriteOfKind(spriteKind: number, range: number, input: Sprite): any {
         let completionvar = 0
         let sprite = null
         let value = null
-        for (let value of spriteutils.getSpritesWithin(SpriteKind.Enemy, range, input)) {
+        for (let value of spriteutils.getSpritesWithin(spriteKind, range, input)) {
             if (scene.spritePercentPathCompleted(value) > completionvar) {
                 completionvar = scene.spritePercentPathCompleted(value)
             }
-            for (let value of sprites.allOfKind(SpriteKind.Enemy)) {
+            for (let value of sprites.allOfKind(spriteKind)) {
                 if (scene.spritePercentPathCompleted(value) >= completionvar) {
                 return value
                 completionvar = 0
