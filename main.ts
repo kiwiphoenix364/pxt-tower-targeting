ab0f51479f8212d6c805a7d9e724d28b2fa1a627
Kiwiphoenix <98428865+kiwiphoenix364@users.noreply.github.com>
2022-02-12 15:32:32 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -5,13 +5,14 @@ namespace Targeting {
         let competionvar = 0
         let sprite = null
         let sprite2 = null
-        let sprite3 = null
         let value = null
         for (let value of spriteutils.getSpritesWithin(SpriteKind.Enemy, range, input)) {
             if (scene.spritePercentPathCompleted(value) > competionvar) {
                 competionvar = scene.spritePercentPathCompleted(value)
                 sprite2 = value
             }
-        return sprite2}
+        return sprite2
+        let completionvar = 0
+        }
     }
 }
