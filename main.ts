1afd346ef34c0b21f4d864ee660827e5b50cb59f
Kiwiphoenix <98428865+kiwiphoenix364@users.noreply.github.com>
2022-02-12 15:25:25 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,13 +2,13 @@
 namespace Targeting {
     //% block="type $type range $range"
     //% type.shadow="kindPicker"
-    export function FindFirstSpriteOfType(spritetype: true, range: number): any {
+    export function FindFirstSpriteOfType(spritetype: true, range: number, input: Sprite): any {
         let competionvar = 0
         let sprite = null
         let sprite2 = null
         let sprite3 = null
         let value = null
-        for (let value of spriteutils.getSpritesWithin(SpriteKind.Enemy, range, sprite2)) {
+        for (let value of spriteutils.getSpritesWithin(SpriteKind.Enemy, range, input)) {
             if (scene.spritePercentPathCompleted(value) > competionvar) {
                 competionvar = scene.spritePercentPathCompleted(value)
                 return sprite2
