830b78577a901d20c6a25014954ffa680b155388
Kiwiphoenix <98428865+kiwiphoenix364@users.noreply.github.com>
2022-02-13 13:01:30 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 //% color="#ff0808"
 namespace Targeting {
-    //% block="Find First Sprite Along Path: Kind $spriteKind Distance $range From Sprite $input "
+    //% block="Find First Sprite Along Path: Kind $spriteKind Max Distance $range From Sprite $input "
     //% spriteKind.shadow="spritekind"
     export function FindFirstSpriteOfKind(spriteKind: number, range: number, input: Sprite): any {
         let completionvar = 0
@@ -15,6 +15,26 @@ namespace Targeting {
                 return value
                 }
             completionvar = 0
+            }
+        }
+    }
+    //% block="Set Sprite $input Velocity Towards First Sprite of Kind $spriteKind On Path Max Distance $range From Sprite Speed $speed"
+    //% spriteKind.shadow="spritekind"
+    export function VelocityTowards(spriteKind: number, range: number, input: Sprite, speed: number) {
+        let completionvar = 0
+        let sprite = null
+        let value = null
+        let ifany = false
+        for (let value of spriteutils.getSpritesWithin(spriteKind, range, input)) {
+            if (scene.spritePercentPathCompleted(value) > completionvar) {
+                ifany = true
+                completionvar = scene.spritePercentPathCompleted(value)
+            }
+            for (let value of sprites.allOfKind(spriteKind)) {
+                if (scene.spritePercentPathCompleted(value) >= completionvar && spriteutils.distanceBetween(value, input) <= range) {
+                spriteutils.setVelocityAtAngle(input, spriteutils.radiansToDegrees(spriteutils.angleFrom(input, value)), speed)
+                }
+                completionvar = 0
             }
         }
     }
